210bde7690594f064574d19c85c099cf76c3068d
Ridan Hawi <ridanhawy@gmail.com>
2023-12-08 22:30:31 +0300

    if command is NULL

--- get_command.c
+++ get_command.c
@@ -1,23 +1,29 @@
 #include "headers.h"
 
 /**
- * get_command - gets the command from the user
- * @command: user command
- * @c_size: command length
- * Return: the user command
- */
+* get_command - gets the command from the user
+* @user_command: user command
+* Return: the user command
+*/
 
 char *get_command(char *user_command)
 {
 	size_t len;
-	
+
+	/*Case where user command is NULL*/
+
+	if (user_command == NULL)
+	{
+		fprintf(stderr, "invalid buffer or size\n");
+	}
+
 	fgets(user_command, 100, stdin);
 	len = strlen(user_command);
 	if (user_command[len - 1] == '\n')
 	{
-		user_command[len - 1] = '\0'
+		user_command[len - 1] = '\0';
 	}
 	return (user_command);
 
-	/*Add case where user command is NULL*/
 }
+
