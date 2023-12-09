7f377a2ddba6438c0002f6e0747aafeea1b0d2c7
lkendi <139500082+lkendi@users.noreply.github.com>
2023-12-09 18:57:18 +0300

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,48 +1,6 @@
 #include "headers.h"
 
-/**
-<<<<<<< HEAD
-=======
-* get_env_var - gets the value of an environment variable
-* @var: name of the environment variable
-* Return: <TBD>
-*/
-
-char *get_env_var(const char *var)
-{
-	if (var == NULL)
-	{
-		return (NULL);
-	}
-
-	extern char **environ;
-	char **current_env;
-	char *token;
-
-	if (var == NULL)
-	{
-		return (NULL);
-	}
-
-	current_env = environ;
-
-	while (current_env != NULL)
-	{
-		token = strtok(*current_env, "=");
-		if (token != NULL)
-		{
-			if (strcmp(var, token) == 0)
-			{
-				return (strtok(NULL, "="));
-			}
-		}
-		current_env++;
-	}
-	return (NULL);
-}
-
-/**
->>>>>>> 1c3c04c875675bb3252eda1f6595adc64ea1e127
+/*
 * execute - execute command
 * @user_command: user command
 */
@@ -50,44 +8,36 @@ void execute(char *user_command)
 {
 	pid_t child;
 	char *args[2];
-<<<<<<< HEAD
-	char *path_val, *path_dup, *p_token, *full_cmd;
-	struct stat b_stat;
-=======
-
-	char *path_val;
-
-	char *token;
->>>>>>> 1c3c04c875675bb3252eda1f6595adc64ea1e127
 
 	if (user_command == NULL)
 	{
 		fprintf(stderr, "Invalid command\n");
 		exit(EXIT_FAILURE);
 	}
-	path_val = get_env_var("PATH");
-	if (path_val != NULL)
-	{
-		path_dup = strdup(path_val);
-	}
-	p_token = strtok(path_dup, ":");
-	while (p_token != NULL)
-	{
-		full_cmd = malloc(strlen(user_command) + strlen(p_token) + 2);
-		strcpy(full_cmd, p_token);
-		strcat(full_cmd, '/');
-		strcat(full_cmd, user_command);
-		strcat(full_cmd, '\0');
 
-		if (stat(full_cmd, &b_stat) == 0)
-		{
-			return (full_cmd);
-			free(path_dup);
-		}
-		else
-		{
-			free(full_cmd);
-			p_token = strtok(NULL, ":");
-		}
-	}	
+	     /*Create a child process*/
+        child = fork();
+        if (child < 0)
+        {
+                perror("fork");
+                exit(EXIT_FAILURE);
+        }
+        else if (child > 0)
+        {
+                /*Parent process*/
+                wait(NULL);
+        }
+        else
+        {
+                args[0] = user_command;
+                args[1] = NULL;
+
+                /*Execute the child process*/
+                if (execve(user_command, args, NULL) == -1)
+                {
+                        fprintf(stderr, "./hsh: %d: %s: not found\n", child, user_command);
+                        exit(EXIT_FAILURE);
+                }
+                exit(EXIT_SUCCESS);
+        }
 }
