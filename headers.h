cf8d366aa36b3a32a381ab0dc5ed37d26da8f156
lkendi <lkendi003@gmail.com>
2023-12-11 23:08:18 +0300

    function prototypes

--- headers.h
+++ headers.h
@@ -10,22 +10,11 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-
-
-/**STRUCTURES*/
-
-
-
-
-
 /**FUNCTION PROTOTYPES*/
-void display_prompt(void);
+void _prompt(void);
 char **get_command(void);
 char *get_command_path(char *user_command);
 void _execute(char **args);
 
-/*not in use currently*/
-char *get_cmd(char *user_command);
-void execute(char *user_command);
-char *get_command_path(char *user_command);
+
 #endif
